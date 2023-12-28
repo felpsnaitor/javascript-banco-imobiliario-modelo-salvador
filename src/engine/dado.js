c0f8ce9a570845a89db09fe24a1291fea427b76d
felpsnaitor <felpsnaitor@gmail.com>
2023-12-28 11:30:10 -0300

    finalizando banco de dados e começando os scripts

---
+++ src/engine/dado.js
@@ -0,0 +1,16 @@
+const dado = [
+    {"id":1, "img-dado":"./src/dados/img/dado-1.png"},
+    {"id":2, "img-dado":"./src/dados/img/dado-2.png"},
+    {"id":3, "img-dado":"./src/dados/img/dado-3.png"},
+    {"id":4, "img-dado":"./src/dados/img/dado-4.png"},
+    {"id":5, "img-dado":"./src/dados/img/dado-5.png"},
+    {"id":6, "img-dado":"./src/dados/img/dado-6.png"}
+]
+console.log(rolandoDados())
+
+async function rolandoDados(){
+    const numeroDoDado = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+    let dadoSelecionado = dado[numeroDoDado-1]
+    
+    return dadoSelecionado
+}
